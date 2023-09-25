0b4bd96b9e47dd34fe62a19ba1ef6c7f8749f755
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2023-09-25 14:41:33 -0700

    hello world

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -6,7 +6,11 @@ import serverless from 'serverless-http';
 const api = express();
 
 const router = Router();
-router.get('/hello', (req, res) => res.send('Hello World!'));
+router.get('/hello', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+  res.send('Hello World!')});
 
 api.use('/api/', router);
 
